d2b38acffd5960024aa1b8760250fb2d50833d8f
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-04-18 21:26:17 -0400

    set up a basic weather api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,19 @@
 class HomeController < ApplicationController
   require 'json'
   def index
-    response = Faraday.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ENV['']')
-    @data = JSON.parse(response.body)
+    if params[:search]
+      #request.variant = :turbo if request.headers["Turbo-Frame"].present?
+      search = params[:search].strip.split(',').join(',')
+      response = Faraday.get("https://api.openweathermap.org/data/2.5/weather?q=#{search}k&APPID=#{ENV['openweather_api_key']}")
+      @data = JSON.parse(response.body)
+      respond_to do |format|
+        format.html 
+        format.turbo_stream
+      end
+    else
+      @data
+    end
+    #@data = response.body
+    #render json: @data
   end
 end
