c67de394ca25b5443a4750195f40953b2d66678f
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-04-18 20:23:22 -0400

    hid secrets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   require 'json'
   def index
-    response = Faraday.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b4d2af7651f1d2fecb58c43621b76ad6')
+    response = Faraday.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ENV['']')
     @data = JSON.parse(response.body)
   end
 end
